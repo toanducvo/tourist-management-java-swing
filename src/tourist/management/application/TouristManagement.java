3ab77d9c648caf37601c1f02d33376c497187b95
ductoanvo <hi.toan@outlook.com.vn>
2021-05-23 01:07:57 +0700

    Update JFrame look and feel

--- src/tourist/management/application/TouristManagement.java
+++ src/tourist/management/application/TouristManagement.java
@@ -2,8 +2,16 @@ package tourist.management.application;
 
 import tourist.management.ui.views.DangNhap;
 
+import javax.swing.*;
+
 public class TouristManagement {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        }
+        catch (Exception ignored) {
+
+        }
         new DangNhap().setVisible(true);
     }
 }
