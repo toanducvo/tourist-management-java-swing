29ec9aaa6139e84be302157466a0c2150dc1e5e2
ductoanvo <hi.toan@outlook.com.vn>
2021-05-24 04:13:05 +0700

    Update UI of GiaoDienKhachHang

--- src/tourist/management/ui/layouts/GiaoDienKhachHang.java
+++ src/tourist/management/ui/layouts/GiaoDienKhachHang.java
@@ -8,20 +8,25 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.format.DateTimeFormatter;
 
 public class GiaoDienKhachHang extends JFrame {
 
     private final DefaultTableModel defaultTableModel;
     private final JTable tblKhachHang;
-    private final JButton btnTimKiem;
+
+    private final JTextField txtTimKiem;
+    private final JButton btnTimKiemTheoTen;
+    private final JButton btnTimKiemTheoCMND;
     private final KhachHangDAO khachHangDAO;
     private final JPanel pnlDanhSach;
     private final JPanel pnlChucNang;
 
+    private static final DateTimeFormatter dateTimeFormatter =  DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
     public GiaoDienKhachHang() {
         setTitle("Danh sách khách hàng");
         setSize(700, 468);
-
         setResizable(false);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -29,7 +34,7 @@ public class GiaoDienKhachHang extends JFrame {
 
         pnlDanhSach = new JPanel();
         pnlDanhSach.setPreferredSize(new Dimension(getWidth(), 360));
-
+        pnlDanhSach.setBorder(BorderFactory.createTitledBorder("Danh Sách khách hàng"));
         Object[] columnName = {
                 "Mã khách hàng",
                 "Họ khách hàng",
@@ -40,10 +45,11 @@ public class GiaoDienKhachHang extends JFrame {
                 "Số điện thoại",
                 "Email"
         };
-
         defaultTableModel = new DefaultTableModel(columnName, 0);
         tblKhachHang = new JTable(defaultTableModel);
         pnlDanhSach.add(new JScrollPane(tblKhachHang));
+        tblKhachHang.setShowGrid(false);
+        tblKhachHang.setRowHeight(20);
         khachHangDAO = new KhachHangDAO();
         for (KhachHang khachHang : khachHangDAO.getAllKhachHang()) {
             defaultTableModel.addRow(
@@ -52,7 +58,7 @@ public class GiaoDienKhachHang extends JFrame {
                             khachHang.getHoKhachHang(),
                             khachHang.getTenKhachHang(),
                             khachHang.isGioiTinh() ? "Nữ" : "Nam",
-                            khachHang.getNgaySinh().toString(),
+                            dateTimeFormatter.format(khachHang.getNgaySinh()),
                             khachHang.getSoCMND(),
                             khachHang.getSoDienThoai(),
                             khachHang.getEmail()
@@ -60,13 +66,29 @@ public class GiaoDienKhachHang extends JFrame {
         }
 
         pnlChucNang = new JPanel();
-        pnlChucNang.setPreferredSize(new Dimension(getWidth(), 50));
+        pnlChucNang.setPreferredSize(new Dimension(getWidth(), 80));
+        pnlChucNang.setBorder(BorderFactory.createTitledBorder("Chức năng"));
+        txtTimKiem = new JTextField(30);
+        btnTimKiemTheoTen = new JButton("Tìm theo tên");
+        btnTimKiemTheoCMND = new JButton("Tìm theo CMND");
+        pnlChucNang.add(txtTimKiem);
+        pnlChucNang.add(btnTimKiemTheoTen);
+        pnlChucNang.add(btnTimKiemTheoCMND);
+
 
         add(pnlDanhSach, BorderLayout.CENTER);
         add(pnlChucNang, BorderLayout.SOUTH);
 
-        btnTimKiem = new JButton("Tìm kiếm");
-        btnTimKiem.addActionListener(new ActionListener() {
+
+        btnTimKiemTheoTen.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+
+        btnTimKiemTheoCMND.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
